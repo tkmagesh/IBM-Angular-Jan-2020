6e5656a18045938e5ab22c9ebef9201b264b6fd7
Magesh Kuppan <tkmagesh77@gmail.com>
2020-01-21 16:59:26 +0530

    angular - first - app

---
+++ first-app/src/app/greeter/greeter.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector : 'app-greeter',
+    templateUrl : 'greeter.component.html',
+    styleUrls : ['greeter.component.css']
+})
+export class GreeterComponent{
+    greetMessage : string = '[Default greet message]';
+
+    onGreetClick(userName : string){
+        this.greetMessage = `Hi ${userName}, Have a nice day!`;
+    }
+}
