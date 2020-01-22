e994eae0b2a3a8ee7ec5c83cffb7d3e7f314074c
Magesh Kuppan <tkmagesh77@gmail.com>
2020-01-22 14:48:57 +0530

    introducing the Bug Model

--- bug-tracker-app/src/app/bugTracker/bugTracker.component.ts
+++ bug-tracker-app/src/app/bugTracker/bugTracker.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 
+interface Bug{
+    name : string,
+    isClosed : boolean
+}
+
 @Component({
     selector : 'app-bug-tracker',
     templateUrl : 'bugTracker.component.html'
 })
 export class BugTrackerComponent{
-    bugs : string[] = [];
+    bugs : Bug[] = [];
 
     onAddNewClick(bugName : string){
-        this.bugs.push(bugName);
+        const newBug : Bug = {
+            name : bugName,
+            isClosed : false
+        } ;
+        this.bugs.push(newBug);
+    }
+
+    onBugNameClick(bug : Bug) : void {
+        bug.isClosed = !bug.isClosed;
     }
 }
