cd55d4ff69a9a069fe8de007448a32f50f4f18d5
Magesh Kuppan <tkmagesh77@gmail.com>
2020-01-22 12:49:00 +0530

    bug tracker template

--- first-app/src/app/products/products.component.ts
+++ first-app/src/app/products/products.component.ts
@@ -5,6 +5,9 @@ import { Component } from '@angular/core';
     template : `
         <h3>Products</h3>
         <hr>
+        <label>Product Name :</label>
+        <input type="text" />
+        <input type="button" value="Add New" />
         <ol>
            <li *ngFor="let product of productsList">{{product}}</li>
         </ol>
@@ -12,7 +15,5 @@ import { Component } from '@angular/core';
 })
 export class ProductsComponent{
     productsList : string[] = [
-        'Pen',
-        'Pencil'
     ]
 }
