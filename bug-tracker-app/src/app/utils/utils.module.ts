9068947ff44a4aab59f13e66a99f1e00bb3a931b
Magesh Kuppan <tkmagesh77@gmail.com>
2020-01-23 14:29:18 +0530

    introducing 'utils module'

---
+++ bug-tracker-app/src/app/utils/utils.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+
+import { TrimTextPipe } from "./pipes/trimText.pipe";
+import { SortPipe } from "./pipes/sort.pipe";
+import { ElapsedPipe } from "./pipes/elapsed.pipe";
+
+@NgModule({
+    declarations : [
+        TrimTextPipe
+        , SortPipe
+        , ElapsedPipe
+    ],
+    exports : [
+        TrimTextPipe
+        , SortPipe
+        , ElapsedPipe
+    ]
+})
+export class UtilsModule{
+
+}
