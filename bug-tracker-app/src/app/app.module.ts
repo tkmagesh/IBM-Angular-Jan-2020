8f02d970f0028ef62c4c55418630eddd6936960d
Magesh Kuppan <tkmagesh77@gmail.com>
2020-01-23 10:50:07 +0530

    ChangeDetection.OnPush implementation

--- bug-tracker-app/src/app/app.module.ts
+++ bug-tracker-app/src/app/app.module.ts
@@ -4,14 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BugTrackerComponent } from './bugTracker/bugTracker.component';
 
+import { BugStatsComponent  } from "./bugTracker/views/bugStats.component";
 import { BugOperationsService } from "./bugTracker/services/bugOperations.service";
 
+import { ClosedCountPipe } from "./bugTracker/pipes/closedCount.pipe";
 import { TrimTextPipe } from "./bugTracker/pipes/trimText.pipe";
 import { SortPipe } from "./bugTracker/pipes/sort.pipe";
 @NgModule({
   declarations: [
     AppComponent
     , BugTrackerComponent
+    , BugStatsComponent
+    , ClosedCountPipe
     , TrimTextPipe
     , SortPipe
   ],
